d1bd5dd021c4022a9e7c77a0953188cf6e9a058b
duducg <edu.sang.rocha@gmail.com>
2024-04-12 17:27:42 +0100

    Added Sphere class

---
+++ ColorSpheres/Sphere.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ColorSpheres
+{
+    public class Sphere
+    {
+        private Color color;
+        private float raio;
+        private int timesTrown;
+
+        public Sphere (Color color,float raio)
+        {
+            this.color = color;
+            this.raio = raio;
+            timesTrown = 0;        
+        } 
+        public void Pop()
+        {
+            raio = 0;
+        }
+        public void Throw()
+        {
+            if (raio > 0)
+            {
+                timesTrown++;   
+            }            
+        }
+        public int GetTimesThrown()
+        {
+            return timesTrown;
+        }
+    }
+}
