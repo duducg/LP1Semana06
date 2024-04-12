a16d5c5c0ea122905e6e9f82c9db6cf27a4f1d13
duducg <edu.sang.rocha@gmail.com>
2024-04-12 18:31:20 +0100

    Better Color class with properties

---
+++ BetterColorSpheres/Color.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BetterColorSpheres
+{
+    public class Color
+    {
+        public byte Red { get;}
+        public byte Green { get; }
+        public byte Blue { get; }
+        public byte Alpha { get; } = 255;
+        public byte Grey => (byte)((Red + Green + Blue) / 3);
+
+        public Color(byte red, byte green, byte blue, byte alpha)
+        {
+            Red =   red;
+            Green = green;
+            Blue = blue;
+            Alpha = alpha;
+
+        }
+        //Constructor overload that set alpha to 255
+        public Color(byte red, byte green, byte blue)
+        {
+            Red =   red;
+            Green = green;
+            Blue = blue;
+            //alpha is initialized to 255 by default            
+
+        }
+
+        
+        
+        
+    }
+}
